d3ca6bf36be97a306f18873981f29c6f67143691
leonardus24 <leonarduskris22@gmail.com>
2025-08-26 23:39:13 +0700

    Update config, login test, dan tambah screenshots

--- orangehrm-cypress/cypress.config.js
+++ orangehrm-cypress/cypress.config.js
@@ -1,11 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 20000,
+    responseTimeout: 20000,
+    pageLoadTimeout: 60000,
+    chromeWebSecurity: false,
+    retries: { runMode: 1, openMode: 0 },
     setupNodeEvents(on, config) {
     },
-    baseUrl: "https://opensource-demo.orangehrmlive.com",
-    viewportWidth: 1280,
-    viewportHeight: 720,
   },
 });
--- orangehrm-cypress/cypress/e2e/login.cy.js
+++ orangehrm-cypress/cypress/e2e/login.cy.js
@@ -1,67 +1,57 @@
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false;
-});
+describe('OrangeHRM Login Feature with Intercept', () => {
 
-describe('OrangeHRM - Login Feature', () => {
+  beforeEach(() => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.intercept('POST', '**/auth/validate').as('loginRequest')
+  })
 
   it('TC01 - Login dengan username dan password valid', () => {
-    cy.visit('/web/index.php/auth/login');
+    cy.get('input[name="username"]').type('Admin')
+    cy.get('input[name="password"]').type('admin123')
+    cy.get('button[type="submit"]').click()
 
-    cy.get('input[name="username"]').should('be.visible').type('Admin');
-    cy.get('input[name="password"]').should('be.visible').type('admin123');
-    cy.get('button[type="submit"]').should('be.visible').click();
+    cy.wait('@loginRequest').its('response.statusCode')
+    .should('be.oneOf', [200, 302])
 
-    cy.url().should('include', '/dashboard');
+    cy.url().should('include', '/dashboard')
+  })
 
-    cy.get('.oxd-userdropdown-tab').should('be.visible').click();
-    cy.contains('Logout').should('be.visible').click();
-    cy.url().should('include', '/auth/login');
-  });
 
-  it('TC02 - Login dengan username salah dan password benar', () => {
-    cy.visit('/web/index.php/auth/login');
+  it('TC02 - Login dengan username salah', () => {
+    cy.get('input[name="username"]').type('SalahUser')
+    cy.get('input[name="password"]').type('admin123')
+    cy.get('button[type="submit"]').click()
 
-    cy.get('input[name="username"]').should('be.visible').type('sanbercode');
-    cy.get('input[name="password"]').should('be.visible').type('admin123');
-    cy.get('button[type="submit"]').should('be.visible').click();
+    cy.wait('@loginRequest')
+    cy.get('.oxd-alert-content').should('contain', 'Invalid credentials')
+  })
 
-    cy.get('.oxd-alert-content-text').should('contain.text', 'Invalid credentials');
-  });
+  it('TC03 - Login dengan password salah', () => {
+    cy.get('input[name="username"]').type('Admin')
+    cy.get('input[name="password"]').type('salah123')
+    cy.get('button[type="submit"]').click()
 
-  it('TC03 - Login dengan username benar dan password salah', () => {
-    cy.visit('/web/index.php/auth/login');
+    cy.wait('@loginRequest')
+    cy.get('.oxd-alert-content').should('contain', 'Invalid credentials')
+  })
 
-    cy.get('input[name="username"]').should('be.visible').type('Admin');
-    cy.get('input[name="password"]').should('be.visible').type('afmin123');
-    cy.get('button[type="submit"]').should('be.visible').click();
+  it('TC04 - Login dengan username kosong', () => {
+    cy.get('input[name="password"]').type('admin123')
+    cy.get('button[type="submit"]').click()
 
-    cy.get('.oxd-alert-content-text').should('contain.text', 'Invalid credentials');
-  });
+    cy.get('.oxd-input-group__message').should('contain', 'Required')
+  })
 
-  it('TC04 - Login tanpa mengisi username dan password', () => {
-    cy.visit('/web/index.php/auth/login');
+  it('TC05 - Login dengan password kosong', () => {
+    cy.get('input[name="username"]').type('Admin')
+    cy.get('button[type="submit"]').click()
 
-    cy.get('button[type="submit"]').should('be.visible').click();
+    cy.get('.oxd-input-group__message').should('contain', 'Required')
+  })
 
-    cy.get('.oxd-input-group__message').should('contain.text', 'Required');
-  });
+  it('TC06 - Login dengan username dan password kosong', () => {
+    cy.get('button[type="submit"]').click()
+    cy.get('.oxd-input-group__message').should('contain', 'Required')
+  })
 
-  it('TC05 - Login tanpa mengisi username', () => {
-    cy.visit('/web/index.php/auth/login');
-
-    cy.get('input[name="password"]').should('be.visible').type('admin123');
-    cy.get('button[type="submit"]').should('be.visible').click();
-
-    cy.get('.oxd-input-group__message').should('contain.text', 'Required');
-  });
-
-  it('TC06 - Login tanpa mengisi password', () => {
-    cy.visit('/web/index.php/auth/login');
-
-    cy.get('input[name="username"]').should('be.visible').type('Admin');
-    cy.get('button[type="submit"]').should('be.visible').click();
-
-    cy.get('.oxd-input-group__message').should('contain.text', 'Required');
-  });
-
-});
+})
